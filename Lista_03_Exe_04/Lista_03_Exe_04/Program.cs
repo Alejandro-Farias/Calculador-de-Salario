9c7196d653887e405454a45d582a7f776130b476
Alejandro <50925169+Alejandro-Farias@users.noreply.github.com>
2019-05-22 16:03:41 -0300

    Programa
    Programa finalizado

---
+++ Lista_03_Exe_04/Lista_03_Exe_04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_03_Exe_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NH, ST = 0;
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Write("Digite o total de horas trabalhadas no dia {0}: ",i+1);
+                NH = int.Parse(Console.ReadLine());
+                NH = NH * 10;
+                ST = ST + NH;
+            }
+            Console.WriteLine("O Salario total é: {0:c}", ST);
+            Console.ReadKey();
+        }
+    }
+}
